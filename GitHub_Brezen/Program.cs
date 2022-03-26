c6592ff4cb67c5a02eb99b47d69ba73073ede344
Josef Navrátil <josef.navratil@educhem.cz>
2022-03-26 22:26:43 +0100

    Přidejte soubory projektu.

---
+++ GitHub_Brezen/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+Console.Write("Zadejte 1. číslo: ");
+int cislo1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Zadejte 2. číslo: ");
+int cislo2 = Convert.ToInt32(Console.ReadLine());
+
+if (cislo1 > cislo2)
+{
+    Console.WriteLine("Číslo jdou od největšího po nejmenší:");
+    Console.WriteLine();
+    Console.WriteLine($"1. číslo = {cislo1}");
+    Console.WriteLine($"2. číslo = {cislo2}");
+}
+else if (cislo1 < cislo2)
+{
+    Console.WriteLine("Číslo jdou od největšího po nejmenší:");
+    Console.WriteLine();
+    Console.WriteLine($"2. číslo = {cislo2}");
+    Console.WriteLine($"1. číslo = {cislo1}");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Neznámá chyba");
+    Console.ResetColor();
+
+}
